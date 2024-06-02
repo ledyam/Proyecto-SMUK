9e4d87a6f2a5e76f0c498fd0e690aadcbeb2107f
Ledyam Ramos <82295064+ledyam@users.noreply.github.com>
2024-06-02 17:27:31 -0400

    sdf
    asd

--- Proyecto en Godot/Personaje/Personaje Principal.gd
+++ Proyecto en Godot/Personaje/Personaje Principal.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var sprite_2d = $Sprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -22,7 +23,11 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction < 0 :
+			sprite_2d.flip_h = true
+		else :
+			sprite_2d.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
